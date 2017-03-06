d864cc85cf291b98b2da7e194e996a064089b350
Jean-Marc Boullianne <jboullianne@gmail.com>
2017-03-06 09:29:16 -0700

    Added placeholders for client-side endpoints.

--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,78 @@ router.get('/', (req, res) => {
 
 /* ============ CLIENT-SIDE ENDPOINTS ============ */
 
-
-/* Example Endpoint Of getting information about a bus */
-router.get('/bus', (req, res) => {
-  	res.status(200).json({ bus: [ {id: 1, name: "Bus1"}, {id: 2, name: "Bus2"}]});
+/*	
+	ABOUT: Contains a list of agencies, along with their properties.
+	PARAMS:
+		REQUIRED: NONE
+		OPTIONAL:
+			agencies	(STRING): A list of agency IDs which to retrieve.
+*/
+router.get('/agencies', (req, res) => {
+  	res.status(200).json({"GET" : req.url, success: true});
 });
+
+/*	
+	ABOUT: Contains a list of arrival estimates, separated by stops
+	PARAMS:
+		REQUIRED: (AT LEAST ONE)
+			agencies	(STRING): A list of agency IDs for which to retrieve the arrival estimates, separated by commas. Defaults to __ (University of Rochester)
+			routes 		(STRING): A list of route IDs for which to retrieve the arrival estimates, separated by commas.
+			stops		(STRING): A list of stop IDs for which to retrieve the arrival estimates, separated by commas.
+		OPTIONAL: NONE
+			
+*/
+router.get('/estimates', (req, res) => {
+  	res.status(200).json({"GET" : req.url, success: true});
+});
+
+/*	
+	ABOUT: Contains a list of routes, along with properties of the routes (segments, names, types, is_active, etc...).
+	PARAMS:
+		REQUIRED: NONE
+		OPTIONAL:
+			agencies	(STRING): A list of agency IDs which to retrieve routes for. Defaults to __ (University of Rochester)
+*/
+router.get('/routes', (req, res) => {
+  	res.status(200).json({"GET" : req.url, success: true});
+});
+
+/*	
+	ABOUT: Contains a list of all the segments that are required to visualize a route.
+	PARAMS:
+		REQUIRED: NONE
+		OPTIONAL:
+			agencies	(STRING): A list of agency IDs which to retrieve segments for. Defaults to __ (University of Rochester)
+			routes 		(STRING): A list of route IDs for which to retrieve segments for, seperated by commas.
+*/
+router.get('/segments', (req, res) => {
+  	res.status(200).json({"GET" : req.url, success: true});
+});
+
+/*	
+	ABOUT: Contains a list of stops
+	PARAMS:
+		REQUIRED: NONE
+		OPTIONAL:
+			agencies	(STRING): A list of agency IDs which to retrieve stops for. Defaults to __ (University of Rochester) 
+*/
+router.get('/stops', (req, res) => {
+  	res.status(200).json({"GET" : req.url, success: true});
+});
+
+/*	
+	ABOUT: Contains a list of vehicles, their properties, and their locations.
+	PARAMS:
+		REQUIRED: NONE
+		OPTIONAL:
+			agencies	(STRING): A list of agency IDs for which to retrieve the vehicles. Defaults to __ (University of Rochester) 
+			routes 		(STRING): A list of route IDs for which to retrieve the vehicles, seperated by commas.
+*/
+router.get('/vehicles', (req, res) => {
+  	res.status(200).json({"GET" : req.url, success: true});
+});
+
+/* ============ END CLIENT-SIDE ENDPOINTS ============ */
 
 // 404 Page
 router.get('/*', (req, res) => {	
